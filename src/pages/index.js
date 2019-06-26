8aa1d5ae412fec5bba79fddd2405adbb2e91b3a3
tomoyukikashiro <ksrtmyk@gmail.com>
2019-06-26 13:49:41 +0900

    feat(list): Use suspense

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,45 @@
-import React, { useEffect, useState, useContext } from "react"
+import React, { useEffect, useContext, Suspense } from "react"
 import { navigate } from 'gatsby'
 import * as clipboard from 'clipboard-polyfill'
-import Octokit from '@octokit/rest'
 
 import Layout from "../components/layout"
 import { searchQueryContext } from '../lib/search-query'
 import { tokenContext } from '../lib/token'
+import { getTasks, cachedResults } from '../lib/github-client'
 
-const toHtml = (task) => {
-  const repo = task.repository_url.split('/').pop()
-  const title = `#${task.number} ${task.title}`
-  return (
-    <li key={task.id} className="list-group-item">
-      <span className="text-secondary mr-2">{repo}</span><a href={task.html_url} title={title} target="_blank" rel="noopener noreferrer">{ title }</a>
-      <div>
-        { task.labels.length && task.labels.map(label => (
-          <span key={label.id} className="badge badge-light mr-1" style={ { color: '#fff', backgroundColor: `#${label.color}` } }>{ label.name }</span>
-        )) }
-      </div>
-    </li>
+const TaskList = ({ tasks }) => {
+  const repoName = (task) => task.repository_url.split('/').pop()
+  const title = (task) => `#${task.number} ${task.title}`
+
+  if (!tasks.length) return (
+    <ul className="list-group list-group-flush">
+      <li className="list-group-item">なし</li>
+    </ul>
   )
+  return (
+    <ul className="list-group list-group-flush">
+      { tasks.map(task => (
+        <li key={task.id} className="list-group-item">
+          <span className="text-secondary mr-2">{repoName(task)}</span>
+          <a href={task.html_url} title={title(task)} target="_blank" rel="noopener noreferrer">{ title(task) }</a>
+          <div>
+            { task.labels.length && task.labels.map(label => (
+              <span key={label.id} className="badge badge-light mr-1" style={ { color: '#fff', backgroundColor: `#${label.color}` } }>{ label.name }</span>
+            )) }
+          </div>
+        </li>
+      )) }
+    </ul>
+  )
+}
+
+const TaskListSuspense = ({ token, queries }) => {
+  if (!token) return <></>
+  const tasks = cachedResults.get(queries)
+  if (tasks) {
+    return <TaskList tasks={tasks}/>
+  }
+  throw getTasks(token, queries)
 }
 
 const toMarkdown = (task) => {
@@ -28,79 +48,45 @@ const toMarkdown = (task) => {
   return `[${title}](${task.html_url}) [${repo}]`
 }
 
-
 const IndexPage = () => {
   const { searchQueryState } = useContext(searchQueryContext)
   const { tokenState, setTokenState } = useContext(tokenContext)
-  const [ results, setResults ] = useState(searchQueryState.map(q => ({id: q.id, name: q.name})))
-  let octokit = null
 
   useEffect(() => {
     if (!searchQueryState.length) return navigate('/settings')
-    fetch()
   }, [])
 
   const onClickClipboard = data => {
-    clipboard.writeText(data.items.map(task => (toMarkdown(task))).join('\n'))
+    const tasks = cachedResults.get(data.queryString)
+    if (!tasks) return
+    clipboard.writeText(tasks.map(task => (toMarkdown(task))).join('\n'))
   }
 
-  const getGithubClient = (gt) => {
-    if (octokit) return octokit;
-    octokit = new Octokit({ auth: gt})
-    return octokit
-  }
-
-  const fetch = async e => {
-    if (e) e.preventDefault()
-    if (!tokenState) return;
-
-    const newResults = await Promise.all(searchQueryState.map(async data => {
-      if (data.queryString.length === 1) {
-        const res = await getGithubClient(tokenState).search.issuesAndPullRequests({q: data.queryString[0]})
-        return { id: data.id, name: data.name, items: res.data.items }
-      } else {
-        const items = await Promise.all(data.queryString.map(async query => {
-          const res = await getGithubClient(tokenState).search.issuesAndPullRequests({q: query})
-          return res.data.items
-        }))
-        const uniqItems = items.reduce((result, items) => {
-          const newItem = items.filter(i => !result.find(j => j.id === i.id))
-          return [...result, ...newItem]
-        }, [])
-        return { id: data.id, name: data.name, items: uniqItems }
-      }
-    }))
-
-    setResults(newResults)
+  const fetch = e => {
+    e.preventDefault()
+    setTokenState(e.target.elements.namedItem('gt').value)
   }
 
   return (
     <Layout>
       <div className="mb-3">
-        <form className="form-inline" onSubmit={e => fetch(e)}>
+        <form className="form-inline" onSubmit={fetch}>
           <div className="form-group mr-2 w-25">
-            <input className="form-control w-100" type="password" name="gt" value={tokenState} onChange={e => setTokenState(e.currentTarget.value)}/>
+            <input className="form-control w-100" type="password" name="gt" defaultValue={tokenState}/>
           </div>
           <button className="btn btn-primary">fetch</button>
         </form>
       </div>
 
-      { results.map(data => (
+      { searchQueryState.map(data => (
         <div key={data.id} className="card mb-4">
           <h5 className="card-header d-flex justify-content-between align-items-center">
             <span>{data.name}</span>
             <button className="btn btn-outline-dark" onClick={() => onClickClipboard(data)}>copy as markdown</button></h5>
           <div className="card-body">
-            { data.hasOwnProperty('items')
-              ? !!data.items.length
-                ? <ul className="list-group list-group-flush">
-                  { data.items.map(task => (
-                    toHtml(task)
-                  )) }
-                </ul>
-                : <div>なし</div>
-              : <div>Loading...</div> }
-
+            <Suspense fallback={<>Loading...</>}>
+              <TaskListSuspense token={tokenState} queries={data.queryString}/>
+            </Suspense>
           </div>
         </div>
       )) }
